5fa289c605f4b4d31a863b30ed77a1992f9e18c0
Ringo Wong <Ringo@MacBook-Pro.local>
2020-02-13 13:20:17 -0800

    Add withTracker and check userId for login status

--- imports/ui/routes/index.js
+++ imports/ui/routes/index.js
@@ -1,3 +1,5 @@
+import { Meteor } from "meteor/meteor";
+import { withTracker } from "meteor/react-meteor-data";
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -13,45 +15,31 @@ import AddGoals from "../pages/AddGoals";
 import Friends from "../pages/Friends";
 import Settings from "../pages/Settings";
 import NavBar from "../components/NavBar";
-// import PrivateRoute from "../components/PrivateRoute";
+
 // import FullScreenLoader from "../components/FullScreenLoader";
 
-class Routes extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoggedIn: true
-    };
-  }
+const Routes = props => {
+  return props.userId ? (
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route exact path="/stats" component={Stats} />
+        <Route exact path="/scoreboard" component={Scoreboard} />
+        <Route exact path="/calendar" component={Calendar} />
+        <Route exact path="/goals" component={Goals} />
+        <Route exact path="/addgoals" component={AddGoals} />
+        <Route exact path="/friends" component={Friends} />
+        <Route exact path="/settings" component={Settings} />
+        <Redirect from="*" to="/stats" />
+      </Switch>
+    </Router>
+  ) : (
+    <FullScreenLoader />
+  );
+};
 
-  toggleIsLoggedIn = () => {
-    this.setState({ isLoggedIn: !this.state.isLoggedIn });
+export default withTracker(() => {
+  return {
+    userId: Meteor.userId()
   };
-
-  render() {
-    return this.state.isLoggedIn ? (
-      <Router>
-        <NavBar toggleIsLoggedIn={this.toggleIsLoggedIn} />
-        <Switch>
-          <Route component={NotFound} />
-        </Switch>
-      </Router>
-    ) : (
-      <Router>
-        <NavBar toggleIsLoggedIn={this.toggleIsLoggedIn} />
-        <Switch>
-          <Route exact path="/stats" component={Stats} />
-          <Route exact path="/scoreboard" component={Scoreboard} />
-          <Route exact path="/calendar" component={Calendar} />
-          <Route exact path="/goals" component={Goals} />
-          <Route exact path="/addgoals" component={AddGoals} />
-          <Route exact path="/friends" component={Friends} />
-          <Route exact path="/settings" component={Settings} />
-          <Redirect from="*" to="/stats" />
-        </Switch>
-      </Router>
-    );
-  }
-}
-
-export default Routes;
+})(Routes);
