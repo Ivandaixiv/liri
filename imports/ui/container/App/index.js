dc2e1edcae4d046016fb8fd8280443222b238d4e
Ivan <ivandaixivwork@gmail.com>
2020-02-13 09:26:40 -0800

    first commit

---
+++ imports/ui/container/App/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+// App component - represents the whole app
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Hell World</h1>
+        </header>
+      </div>
+    );
+  }
+}
+export default App;
