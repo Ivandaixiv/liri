b817129ea72ff62e2e5f0818e167db546774703d
ringo-w <ringo.sja@gmail.com>
2020-02-14 11:03:28 -0800

    Add Sign Up component to AccountForm

--- imports/ui/components/AccountForm/AccountForm.js
+++ imports/ui/components/AccountForm/AccountForm.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
-import { Accounts } from "meteor/accounts-base";
 import { Meteor } from "meteor/meteor";
+import { Accounts } from "meteor/accounts-base";
 
 export default class AccountForm extends Component {
+  signup = event => {
+    event.preventDefault();
+    const username = event.target.username.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    Accounts.createUser({ username, email, password }, error =>
+      console.log(error)
+    );
+  };
+
   login = event => {
     event.preventDefault();
     const username = event.target.username.value;
@@ -13,15 +24,25 @@ export default class AccountForm extends Component {
         console.log(error);
       } else {
         props.setLoggingIn(Meteor.loggingIn());
-        window.location.replace("/member-area");
+        window.location.replace("/stats");
       }
     });
   };
-
+  Accounts.createUser({ username, email, password }, error =>
+    console.log(error)
+  );
+};
   render() {
     console.log("Logged In: ", Meteor.userId());
     return (
       <div>
+         <h1 style={{ color: "white" }}>SignUp</h1>
+        <form onSubmit={this.signup}>
+          <input name="username" placeholder="username" type="text" />
+          <input name="email" placeholder="email" type="text" />
+          <input name="password" placeholder="password" type="password" />
+          <button type="submit">Signup</button>
+        </form>
         <h1 style={{ color: "white" }}>Log In</h1>
         <form onSubmit={this.login}>
           <input name="username" placeholder="username" type="text" />
