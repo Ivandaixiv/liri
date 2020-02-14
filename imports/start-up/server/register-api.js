df3066cbdba3c7c0f7459587cb056c432e187c15
Ian <55965026+IED92@users.noreply.github.com>
2020-02-14 11:14:20 -0800

    Refactor register-api

---
+++ imports/start-up/server/register-api.js
@@ -0,0 +1,3 @@
+import users from "../../api/users";
+import tasks from "../../api/tasks";
+import pets from "../../api/pets";
--- server/main.js
+++ server/main.js
@@ -3,3 +3,16 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+<<<<<<< Updated upstream
+=======
+
+// Accounts.onCreateUser(function(options, user) {
+//   if (options.profile) {
+//     user.profile = options.profile;
+//   } else {
+//     user.profile = {};
+//   }
+//   user.profile.friends = [];
+//   return user;
+// });
+>>>>>>> Stashed changes
