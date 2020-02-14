405d6d03ed46069836851809d3cf15d4163480c6
ringo-w <ringo.sja@gmail.com>
2020-02-14 14:35:38 -0800

    Begin adding final form to Login

--- imports/ui/container/App/index.js
+++ imports/ui/container/App/index.js
@@ -1,17 +1,16 @@
 import React, { Component } from "react";
 import LoginContainer from "../../pages/Login";
-import Router from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 // App component - represents the whole app
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        {/* <Router> */}
-        {/* <LoginContainer /> */}
-        Hello
-        {/* </Router> */}
-      </div>
+      <Router>
+        <div className="container">
+          <LoginContainer />
+        </div>
+      </Router>
     );
   }
 }
