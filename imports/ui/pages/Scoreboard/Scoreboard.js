a6b112a834e834fcbb00d174b45c2949ca871a40
Ivan <ivandaixivwork@gmail.com>
2020-02-18 10:27:53 -0800

    Adds temporary Scoreboard page

---
+++ imports/ui/pages/Scoreboard/Scoreboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import AccountForm from "../../components/AccountForm";
+import Scoreboard from "../../components/Scoreboard";
+// import { withStyles } from "@material-ui/core/styles";
+// import styles from "./styles";
+// import PropTypes from "prop-types";
+
+const Login = ({}) => {
+  return <Scoreboard />;
+};
+
+export default Login;
