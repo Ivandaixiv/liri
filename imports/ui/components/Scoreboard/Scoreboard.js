3f932d66d8cf496e6ceaf8b6e977f035b2be8f03
Ian <55965026+IED92@users.noreply.github.com>
2020-02-18 10:42:40 -0800

    Work on scorebaord component

--- imports/ui/components/Scoreboard/Scoreboard.js
+++ imports/ui/components/Scoreboard/Scoreboard.js
@@ -2,6 +2,42 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
 import { Meteor } from "meteor/meteor";
+import { List, ListItem, ListDivider } from "@material-ui/core";
+import Gravatar from "react-gravatar";
 import styles from "./styles";
+
+class Scoreboard extends Component {
+  selectFriend(FriendUserId) {
+    this.props.onFriendSelected(FriendUserId);
+  }
+
+  render() {
+    return (
+      <List>
+        {this.props.users.map(user => {
+          return (
+            <div className={classes.ListItem}>
+              <Gravatar email={user.email} />
+              <ListItem
+                key={user._id}
+                primaryText={user.name}
+                onClick={this.selectFriend.bind(this, user._id)}
+                secondaryText={"Current score: " + user.streak}
+                style={style}
+              />
+              <ListDivider />
+            </div>
+          );
+        })}
+      </List>
+    );
+  }
+}
+
+Scoreboard.propTypes = {
+  selectedUserId: React.PropTypes.string,
+  users: React.PropTypes.array
+  // onPlayerSelected: React.PropTypes.func
+};
 
 export default withRouter(withStyles(styles)(Scoreboard));
