0d63d4ff8b9b93526686267b4d8a283ecc503a59
fostlt <lanceadamp@gmail.com>
2020-02-18 10:37:45 -0800

    Fullscreen loader

--- imports/ui/components/FullScreenLoader/FullScreenLoader.js
+++ imports/ui/components/FullScreenLoader/FullScreenLoader.js
@@ -2,6 +2,7 @@ import React from "react";
 import { withStyles, CardMedia } from "@material-ui/core";
 import styles from "./styles";
 
+
 const FullScreenLoader = props => {
   const { classes } = props;
   return (
@@ -9,7 +10,7 @@ const FullScreenLoader = props => {
       <CardMedia
         className={classes.card}
         component="img"
-        image="https://cdn2.iconfinder.com/data/icons/fitness-and-sports/100/Fitnes-13-512.png"
+        image="../../../src/image/liri.png"
       />
     </div>
   );
