9e81ca5bfba0196e502366a3fd4b1670ca363445
ringo-w <ringo.sja@gmail.com>
2020-02-18 13:53:59 -0800

    Remove ./styles from Scoreboard and logout button from Stats from test environment

--- imports/ui/pages/Stats/Stats.js
+++ imports/ui/pages/Stats/Stats.js
@@ -11,7 +11,6 @@ const Login = ({}) => {
       <Typography variant="h2">Your Stats</Typography>
       <Typography></Typography>
       <img src="../../../src/image/liri.png"></img>
-      <button onClick={Meteor.logout}>Logout</button>
     </div>
   );
 };
