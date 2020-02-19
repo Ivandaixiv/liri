8ee6798c94e0d2a62ad475be3db0c0fbe9cd01c4
Ian <55965026+IED92@users.noreply.github.com>
2020-02-18 16:54:59 -0800

    Fix addFriend method and added input field

--- imports/api/users.js
+++ imports/api/users.js
@@ -1,27 +1,36 @@
 import { Meteor } from "meteor/meteor";
 
+if (Meteor.isServer) {
+  Meteor.publish("users", function usersPublication() {
+    return Meteor.users.find(
+      {},
+      { fields: { profile: 1, username: 1, email: 1 } }
+    );
+  });
+}
 Meteor.methods({
-  "user.findFriend"() {
-    if (Meteor.userId()) {
-      const userIds = Meteor.users
-        .find({})
-        .fetch()
-        .map(user => user.friends);
-      return userIds;
-    }
-  },
+  // "user.findFriend"() {
+  //   if (Meteor.userId()) {
+  //     const userIds = Meteor.user()?.profile?.friends;
+  //     return userIds;
+  //   }
+  // },
   // Method to add friends
-  "user.addFriend"(friendUserId) {
-    if (!friendUserId) {
+  "user.addFriend"(username) {
+    if (!username) {
       throw new Meteor.Error(
         "user.addFriend.not-authorized",
         "Unable to add user to friends list."
       );
     }
 
-    Meteor.users.update(Meteor.userId(), {
-      profile: { $push: { friends: friendUserId } }
-    });
+    const newFriend = Meteor.users.findOne({ username });
+
+    if (newFriend) {
+      Meteor.users.update(Meteor.userId(), {
+        $push: { "profile.friends": newFriend._id }
+      });
+    }
   },
   // Method to remove friends
   "user.removeFriend"(friendUserId) {
